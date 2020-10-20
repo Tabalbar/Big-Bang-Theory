72e328b754c6ac50d83b9bfbc3681ec7b371fb0d
Tabalbar <35385849+Tabalbar@users.noreply.github.com>
2020-10-20 11:25:21 -1000

    Cleaned up start react code and added starter example code for react-three-fiber

---
+++ my-app/src/Components/Visualization.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Visualization(){
+    return (
+        <>
+            <h1>Hello world</h1>
+        </>
+    )
+}
+
+export default Visualization
---
+++ my-app/src/Pages/Main.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Visualization from "../Components/Visualization";
+import { Canvas } from 'react-three-fiber'
+
+import Box from "./Box";
+
+function Main(){
+
+
+    return (
+        <>
+            <Canvas>
+                <ambientLight />
+                <pointLight position={[10, 10, 10]} />
+                <Box position={[-1.2, 0, 0]} />
+                <Box position={[1.2, 0, 0]} />
+            </Canvas>
+        </>
+    )
+}
+
+export default Main
