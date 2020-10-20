eefeb3eb58898773ddc493a8ace561bad669f6c7
Tabalbar <35385849+Tabalbar@users.noreply.github.com>
2020-10-20 13:02:34 -1000

    added template for planets

--- my-app/src/Pages/Box.js
+++ my-app/src/Pages/Box.js
@@ -6,8 +6,8 @@ function Box(props) {
     const mesh = useRef()
 
     // Set up state for the hovered and active state
-    const [hovered, setHover] = useState(false)
-    const [active, setActive] = useState(false)
+    const [hovered, setHover] = useState(false);
+    const [active, setActive] = useState(false);
 
     // Rotate mesh every frame, this is outside of React without overhead
     useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
@@ -16,13 +16,13 @@ function Box(props) {
         <mesh
             {...props}
             ref={mesh}
-            scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+            scale={active ? [2, 2, 2] : [1, 1, 1]}
             onClick={(e) => setActive(!active)}
             onPointerOver={(e) => setHover(true)}
             onPointerOut={(e) => setHover(false)}
         >
-            <boxBufferGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+            <boxBufferGeometry attach='geometry' args={[1, 1, 1]} />
+            <meshStandardMaterial attach='material' color={hovered ? 'hotpink' : 'orange'} />
         </mesh>
     )
 }
--- my-app/src/Pages/Main.js
+++ my-app/src/Pages/Main.js
@@ -1,21 +1,131 @@
 import React from "react";
 import Visualization from "../Components/Visualization";
 import { Canvas } from 'react-three-fiber'
-
+import {OrbitControls} from "drei";
 import Box from "./Box";
+import '../style.css'
+import Sphere from "./Sphere";
+import _ from "lodash"
 
 function Main(){
 
+    const planetInfo = [
+        {
+            color: 'yellow',
+            size: [4,4,4],
+            position: [0,0,0]
+        },
+        {
+            color: 'grey',
+            size: [.5,.5,.5],
+            position: [5,0,0]
+        },
+        {
+            color: 'brown',
+            size: [.9,.9,.9],
+            position: [10,0,0]
+        },
+        {
+            color: 'blue',
+            size: [1,1,1],
+            position: [15,0,0]
+        },
+        {
+            color: 'red',
+            size: [.5,.5,.5],
+            position: [20,0,0]
+        },
+        {
+            color: 'brown',
+            size: [3,3,3],
+            position: [25,0,0]
+        },
+        {
+            color: 'brown',
+            size: [2,2,2],
+            position: [30,0,0]
+        },
+        {
+            color: 'green',
+            size: [1.5,1.5,1.5],
+            position: [35,0,0]
+        },
+        {
+            color: 'blue',
+            size: [2.5,2.5,2.5],
+            position: [40,0,0]
+        }
+    ];
+
+    // const planetInfo = [
+    //     {
+    //         color: 'yellow',
+    //         size: [400,400,400],
+    //         position: [0,0,0]
+    //     },
+    //     {
+    //         color: 'grey',
+    //         size: [3.8,3.8,3.8],
+    //         position: [380,0,0]
+    //     },
+    //     {
+    //         color: 'brown',
+    //         size: [9.5,9.5,9.5],
+    //         position: [720,0,0]
+    //     },
+    //     {
+    //         color: 'blue',
+    //         size: [10,10,10],
+    //         position: [1000,0,0]
+    //     },
+    //     {
+    //         color: 'red',
+    //         size: [5.3,5.3,5.3],
+    //         position: [1500,0,0]
+    //     },
+    //     {
+    //         color: 'brown',
+    //         size: [110,110,110],
+    //         position: [5200,0,0]
+    //     },
+    //     {
+    //         color: 'brown',
+    //         size: [90.4,90.4,90.4],
+    //         position: [9500,0,0]
+    //     },
+    //     {
+    //         color: 'green',
+    //         size: [40,40,40],
+    //         position: [19000,0,0]
+    //     },
+    //     {
+    //         color: 'blue',
+    //         size: [38,38,38],
+    //         position: [30000,0,0]
+    //     }
+    // ];
 
     return (
-        <>
-            <Canvas>
+        <div className='mainVisualization'>
+            <Canvas
+                camera={{far: 10000}}
+            >
                 <ambientLight />
                 <pointLight position={[10, 10, 10]} />
-                <Box position={[-1.2, 0, 0]} />
-                <Box position={[1.2, 0, 0]} />
+                {
+                    _.times(planetInfo.length, (i) => (
+                        <Sphere
+                            color={planetInfo[i].color}
+                            size={planetInfo[i].size}
+                            position={planetInfo[i].position}
+                        />
+                    ))
+                }
+                {/*<Box position={[-1.2, 0, 0]} />*/}
+                {/*<Box position={[1.2, 0, 0]} />*/}
+                <OrbitControls/>
             </Canvas>
-        </>
+        </div>
     )
 }
 
