211a08b112929cc72f9c301856811e1111742b35
Tabalbar <35385849+Tabalbar@users.noreply.github.com>
2020-11-19 16:41:39 -1000

    updated csv reader

--- my-app/src/CSVReader.js
+++ my-app/src/CSVReader.js
@@ -5,21 +5,26 @@ import fileDownload from "js-file-download";
 
 function CSVReader() {
 
-    const [csvFile, setCsvFile] = useState(null);
+    // const [csvFile, setCsvFile] = useState(null);
 
     const handleReadCSV = async () => {
         let arr = [];
-        await d3.csv(csvFile,  (data) => {
+        await d3.csv(xyzCoordinates,  (data) => {
             const JSONobj =  JSON.stringify(data);
             arr.push(JSONobj)
         })
         fileDownload(arr, 'xyzCoordinates.json')
-    }
+    };
+
+    // const handleChange = (event)=>{
+    //     setCsvFile(event.target.files[0])
+    //     console.log(event.target.files[0])
+    // }
 
     return (
         <>
             <h1>CsvReader</h1>
-            <input type='file'/>
+            {/*<input type='file' onChange={handleChange}/>*/}
             <button onClick={handleReadCSV}>read csv</button>
         </>
     )
