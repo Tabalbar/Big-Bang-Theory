0cb7e7afec0a826b976c25c7615a123b48e9dd85
Christopher Nishimura <42785017+chris-nishimura@users.noreply.github.com>
2020-12-07 20:02:38 -1000

    changed text in legend

--- my-app/src/Components/Toolbar.js
+++ my-app/src/Components/Toolbar.js
@@ -207,13 +207,13 @@ function Toolbar(
                 <div className="grid-item-bw">Blue White</div>
                 <div className="grid-item-b">Blue</div>
                 <div className="grid-item">Surface Temp</div>
-                <div className="grid-item">less than 3,700 K</div>
+                <div className="grid-item"> &lt; 3,700 K</div>
                 <div className="grid-item">3,700-5,200 K</div>
                 <div className="grid-item">5,200-6,000 K</div>
                 <div className="grid-item">6,000-7,500 K</div>
                 <div className="grid-item">7,500-10,000K</div>
                 <div className="grid-item">10,000-30,000K</div>
-                <div className="grid-item">greater than 33,000K</div>
+                <div className="grid-item">&gt; 33,000K</div>
             </div>
 
             <div>
